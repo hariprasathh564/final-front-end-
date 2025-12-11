b2423bafc170d33a8eaf1ec55e72b19ca0909d2b
hariprasath <hariprasathh564@gmail.com>
2025-12-11 11:31:59 +0530

    change

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -18,7 +18,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "REACT_APP_STRIPE_PUBLISHABLE_KEY=pk_test_51SPdM7CQopk_test_51STPCD2Eg5btMSJ60lv34YB6B0FjxgJQxlI765l9LbrOsWsiFsMhy7KN85Dcso93ecpywCfRbwGX7av3UxsLXTRW00uu8POD9oKwJXOcwuWa5LJtuw1a60fhIy36CVJ8vw3jzOkJIBFaIiV3DhYtvSo0TOmHBRrlpx5WUrGwLpsxJdjQ00piSYZiuf/api", 
+  baseURL: "https://final-back-end-2.onrender.com/api", 
   headers: {
     "Content-Type": "application/json",
   },
