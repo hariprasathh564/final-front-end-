2125d4ca9ab4e0ad62f6a61e519aad5699f0ff4e
hariprasath <hariprasathh564@gmail.com>
2025-12-16 09:47:25 +0530

    sip different changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -198,7 +198,7 @@ export default function Home() {
                    initial={{ y: 100, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.8, ease: [0.16, 1, 0.3, 1] }}
                    className="text-5xl md:text-7xl font-black text-blue tracking-tighter drop-shadow-lg leading-[0.9]"
                  >
-                   SIP <br/> DIFFERENT.
+                   SIP DIFFERENT.
                  </motion.h1>
                </div>
            </div>
